a58512538cf51a1c30c5456aaf070ead50def985
PeterSo123 <somanlung15@gmail.com>
2018-04-15 21:58:52 +0800

    Add files via upload

---
+++ html/home.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>Movie.com</title>
+	<link rel="shortcut icon" type="image/x-icon" href="css/images/favicon.ico" />
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+	<link rel="stylesheet" href="css/colorbox.css" type="text/css" media="all" />
+	
+	<script src="js/jquery-1.8.0.min.js" type="text/javascript"></script>
+	<script src="js/jquery.colorbox-min.js" type="text/javascript"></script>
+	<!--[if lt IE 9]>
+		<script src="js/modernizr.custom.js"></script>
+	<![endif]-->
+	<script src="js/functions.js" type="text/javascript"></script>
+</head>
+<body>
+	<!-- wrapper -->
+	<div id="wrapper">
+		<div class="light-bg">
+			<!-- shell -->
+			<div class="shell">
+				<!-- header -->
+				<div class="header">
+					<!-- socials -->
+					<div class="socials">
+						<a href="#" class="facebook-ico">facebook-ico</a>
+						<a href="#" class="twitter-ico">twitter-ico</a>
+						<a href="#" class="you-tube-ico">you-tube-ico</a>
+					</div>
+					<!-- end of socials -->
+					<h1 id="logo"><a href="#">Moviereviews</a></h1>
+					<!-- navigation -->
+					<nav id="navigation">
+						<ul>
+							<li><a href="#">SHOW ALL</a></li>
+							<li><a href="#">LATEST MOVIES <span>20</span></a></li>
+							<li><a href="#">TOP RATED</a></li>
+							<li><a href="#">MOST COMMENTED</a></li>
+						</ul>
+					</nav>
+					<!-- end of navigation -->
+					<div class="cl">&nbsp;</div>
+				</div>
+				<!-- end of header -->
+				<!-- main -->
+				<div class="main">
+					<!-- content -->
+					<section class="content">
+						<!-- post -->
+						<div class="post">
+							<!-- post-inner -->
+							<div class="post-inner">
+								<header>
+									<h2><a href="#"><?php 
+                                                        include("data selecct"); 
+                                                    ?>
+                                        </a></h2>
+									<div class="cl">&nbsp;</div>
+								</header>
+								<div class="img-holder">
+									<a href="#"><img src="css/images/Rampage.jpg" alt="" height="730" width="633" /></a>
+									<a target="_blank" href="css/images/Rampage.jpg" class="btn-full-image popup"><span>FULL IMAGE</span></a>
+								</div>
+								
+								<!-- meta -->
+								<div class="meta">
+									<p class="date">APRIL 13, 2018</p>
+									<div class="cl">&nbsp;</div>
+								</div>
+								<!-- end of meta -->
+								<!-- post-cnt -->
+								<div class="post-cnt">
+									<p>Primatologist Davis Okoye (Johnson), a man who keeps people at a distance, shares an unshakable bond with George, the extraordinarily intelligent, silverback gorilla who has been in his care since birth. But a rogue genetic experiment gone awry mutates this gentle ape into a raging creature of enormous size. To make matters worse, it's soon discovered there are other similarly altered animals. As these newly created alpha predators tear across North America, destroying everything in their path, Okoye teams with a discredited genetic engineer to secure an antidote, fighting his way through an ever-changing battlefield, not only to halt a global catastrophe but to save the fearsome creature that was once his friend.</p>
+									<a href="Rampage" class="more">See Ranking</a>
+								</div>
+								<!-- end of post-cnt -->
+							</div>
+							<!-- post-inner -->
+						</div>
+						<!-- end of post -->
+						<!-- post -->
+						<div class="post">
+							<!-- post-inner -->
+							<div class="post-inner">
+								<header>
+									<h2><a href="#"><?php 
+                                                        include("data selecct"); 
+                                                    ?>
+                                        </a></h2>
+									<div class="cl">&nbsp;</div>
+								</header>
+								<div class="img-holder">
+									<a href="#"><img src="css/images/Ready-Player-One-posters-7-1.jpg" alt="" height="730" width="633" /></a>
+									<a href="css/images/Ready-Player-One-posters-7-1.jpg" class="btn-full-image popup"><span>FULL IMAGE</span></a>
+								</div>
+								
+								<!-- meta -->
+								<div class="meta">
+									<p class="date">APRIL 09, 2012 by <a href="#">JOHN DOE</a></p>
+									<div class="cl">&nbsp;</div>
+								</div>
+								<!-- end of meta -->
+								<!-- post-cnt -->
+								<div class="post-cnt">
+									<p>In the year 2045, people can escape their harsh reality in the OASIS, an immersive virtual world where you can go anywhere, do anything, be anyone-the only limits are your own imagination. OASIS creator James Halliday left his immense fortune and control of the Oasis to the winner of a contest designed to find a worthy heir. When unlikely hero Wade Watts conquers the first challenge of the reality-bending treasure hunt, he and his friends-known as the High Five-are hurled into a fantastical universe of discovery and danger to save the OASIS and their world.</p>
+									<a href="#" class="more">CONTINUE READING</a>
+								</div>
+								<!-- end of post-cnt -->
+							</div>
+							<!-- post-inner -->
+						</div>
+						<!-- end of post -->
+						<!-- post -->
+						<div class="post">
+							<!-- post-inner -->
+							<div class="post-inner">
+								<header>
+									<h2><a href="#">MovieTitle 3 Goes Here</a></h2>
+									<div class="cl">&nbsp;</div>
+								</header>
+								<div class="img-holder">
+									<a href="#"><img src="css/images/post-img3.jpg" alt="" /></a>
+									<a target="_blank" href="css/images/post-img3.jpg" class="btn-full-image popup"><span>FULL IMAGE</span></a>
+								</div>
+								
+								<!-- meta -->
+								<div class="meta">
+									<p class="date">APRIL 09, 2012 by <a href="#">JOHN DOE</a></p>
+									<div class="right">
+										<div class="rating-holder">
+											<p>RATING</p>
+											<div class="rating">
+												<span style="width: 70%;"></span>
+											</div>
+										</div>
+										<a href="#" class="comments">59 comments</a>
+									</div>
+									<div class="cl">&nbsp;</div>
+								</div>
+								<!-- end of meta -->
+								<!-- post-cnt -->
+								<div class="post-cnt">
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut commodo orci vitae enim volutpat dapibus. Aliquam pharetra, odio eu luctus semper, dolor libero volutpat mi, sit amet convallis tortor justo eu nisl. Cras quis felis quis felis tempor volutpat id at dui. Donec vel turpis id odio tempus mollis quis vel justo. Etiam tortor nisi, pulvinar a mattis nec, luctus sit amet odio. Morbi id commodo ligula. </p>
+									<a href="#" class="more">CONTINUE READING</a>
+								</div>
+								<!-- end of post-cnt -->
+							</div>
+							<!-- post-inner -->
+						</div>
+						<!-- end of post -->
+						<div class="pagination">
+							<ul>
+								<li class="laquo"><a href="#"><span></span></a></li>
+								<li class="active"><a href="#">1</a></li>
+								<li><a href="#">2</a></li>
+								<li><a href="#">3</a></li>
+								<li><a href="#">4</a></li>
+								<li><a href="#">5</a></li>
+								<li class="raquo"><a href="#"><span></span></a></li>
+								<li><a href="#">10</a></li>
+								<li class="dots">. . .</li>
+								<li><a href="#">last</a></li>
+							</ul>
+						</div>
+					</section>
+					<!-- end of content -->
+					<!-- sidebar -->
+				
+					<!-- end of sidebar -->
+					<div class="cl">&nbsp;</div>
+				</div>
+				<!-- end of main -->
+				<div class="footer">
+					<nav class="footer-nav">
+						<ul>
+							<li><a href="#">Show All</a></li>
+							<li><a href="#">Latest Movies</a></li>
+							<li><a href="#">Top Rated</a></li>
+							<li><a href="#">Most Commented</a></li>
+						</ul>
+					</nav>
+					<p class="copy">Copyright &copy; 2012 <span>|</span> Sitename. Design by <a href="http://chocotemplates.com" target="_blank">ChocoTemplates.com</a></p>
+				</div>
+			</div>
+			<!-- end of shell -->
+		</div>	
+	</div>
+	<!-- end of wrapper -->
+</body>
+</html>
